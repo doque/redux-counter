20643c116c76042a5014e18e7a66a0cd283cc052
Dominique Busser <dominique.busser@netlight.com>
2017-07-05 13:24:17 +0200

    Tests for Action Creators and Counter

---
+++ src/actions/counterActions.test.js
@@ -0,0 +1,14 @@
+import * as types from "../constants/counterActionTypes";
+import { increment, decrement } from "./counterActions";
+
+describe("counter action creator", function() {
+  it("creates the correct action increment", () => {
+    const expected = {
+      type: types.INCREMENT
+    };
+
+    expect(increment()).toEqual(expected);
+  });
+
+  it("creates the correct action decrement", () => {});
+});
